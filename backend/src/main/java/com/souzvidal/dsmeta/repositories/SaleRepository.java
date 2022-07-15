06c2224a01c353c943f1505761c8cc9ec7e9918d
Daniel Amancio Vidal de Souza <daniel.avsouza@sp.senac.br>
2022-07-15 11:45:30 -0300

    Api test

---
+++ backend/src/main/java/com/souzvidal/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.souzvidal.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.souzvidal.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
---
+++ backend/src/main/java/com/souzvidal/dsmeta/services/SaleService.java
@@ -0,0 +1,23 @@
+package com.souzvidal.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.souzvidal.dsmeta.entities.Sale;
+import com.souzvidal.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		
+		return repository.findAll();
+		
+	}
+
+}
